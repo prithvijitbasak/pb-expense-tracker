540f48d0cd653c76c7baae6b4b51891a001ed3ac
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-04-13 16:58:55 +0530

    showing edit time only when the expense is edited

--- frontend/src/components/AllDetailsCard.jsx
+++ frontend/src/components/AllDetailsCard.jsx
@@ -1,17 +1,32 @@
 import PropTypes from "prop-types";
 import "../assets/styles/AllDetailsCard.css";
 import { formatDateTime, formatDtConven } from "../utils/formatterFunctions";
+import { useEffect, useState } from "react";
 
 const AllDetailsCard = ({ index, expense, checkIsEditClicked }) => {
   const { title, amount, category, date, notes, createdAt, updatedAt } =
     expense;
 
+  const [editTime, showEditTime] = useState(false);
+
   const handleEditClick = () => {
     checkIsEditClicked(expense); // Pass the whole expense to parent
   };
 
+  const checkIfEdited = (createdAt, updatedAt) => {
+    if (new Date(createdAt).getTime() !== new Date(updatedAt).getTime()) {
+      showEditTime(true);
+    } else {
+      showEditTime(false);
+    }
+  };
+
+  useEffect(() => {
+    checkIfEdited(createdAt, updatedAt);
+  }, [createdAt, updatedAt]);
+
   return (
-    <div className="expense-card flex flex-col justify-between" key={index}>
+    <div className="expense-card flex flex-col justify-between gap-4" key={index}>
       <div className="pb-3">
         <p className="title">
           <span className="font-bold tracking-wider">Title:</span> {title}
@@ -33,24 +48,31 @@ const AllDetailsCard = ({ index, expense, checkIsEditClicked }) => {
           <span className="font-bold tracking-wider">Added on:</span>{" "}
           {formatDateTime(createdAt)}
         </p>
-        <p className="updated-date">
-          <span className="font-bold tracking-wider">Updated on:</span>{" "}
-          {formatDateTime(updatedAt)}
-        </p>
       </div>
-      <div className="edit-delete-btn flex justify-end gap-4">
-        <button
-          className="rounded bg-green-500 px-2 font-medium cursor-pointer edit-btn"
-          onClick={() => {
-            handleEditClick();
-            console.log("opening the edit modal");
-          }}
-        >
-          Edit
-        </button>
-        <button className="rounded bg-red-500 px-3 py-3 font-medium cursor-pointer delete-btn">
-          Delete
-        </button>
+      <div
+        className={`edit-delete-btn flex ${
+          editTime ? "justify-between" : "justify-end"
+        } gap-4 items-end`}
+      >
+        {editTime && (
+          <p className="text-xs font-normal">
+            Last edited at: {formatDateTime(updatedAt)}{" "}
+          </p>
+        )}
+        <div className="flex justify-between gap-3">
+          <button
+            className="rounded bg-green-500 px-2 font-medium cursor-pointer edit-btn"
+            onClick={() => {
+              handleEditClick();
+              console.log("opening the edit modal");
+            }}
+          >
+            Edit
+          </button>
+          <button className="rounded bg-red-500 px-3 py-3 font-medium cursor-pointer delete-btn">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
