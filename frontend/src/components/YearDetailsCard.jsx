d21f288968ee6f624e0d19585f6f394c25db7aee
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-04-27 09:14:56 +0530

    fetched the total expense of the current month in the dashboard card

--- frontend/src/components/YearDetailsCard.jsx
+++ frontend/src/components/YearDetailsCard.jsx
@@ -1,11 +1,45 @@
 import { Link } from "react-router-dom";
 import "../assets/styles/DetailsCard.css";
+import { API } from "../utils/auth";
+import { useEffect, useState } from "react";
+
 const YearDetailsCard = () => {
+  const [totalExpense, setTotalExpense] = useState(null);
+
+  const fetchTotalYearExpense = async () => {
+    const token = localStorage.getItem("token");
+    const currentYear = new Date().getFullYear();
+    try {
+      const response = await fetch(
+        `${API}/api/expenses/get-expenses-by-year?year=${currentYear}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        const totalExpenses = data.totalExpenses;
+        setTotalExpense(totalExpenses);
+        console.log(totalExpenses);
+      }
+    } catch (error) {
+      console.error("The error is: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTotalYearExpense();
+  }, []);
+
   return (
     <div className="details-card">
       <h3 className="details-heading-text">This Year Expenses</h3>
-      <p className="total-text">Total Expenses = </p>
-      <Link to={"/year-details"} className="details-btn">See Details</Link>
+      <p className="total-text">Total Expenses = {totalExpense}</p>
+      <Link to={"/year-details"} className="details-btn">
+        See Details
+      </Link>
     </div>
   );
 };
