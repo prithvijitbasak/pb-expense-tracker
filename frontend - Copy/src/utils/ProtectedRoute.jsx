4991b07c996ab896ff787cf6f3bf3a9448f2b600
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-06-01 16:42:21 +0530

    Remove frontend-Copy from tracking and add to .gitignore

--- frontend - Copy/src/utils/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated } from "../utils/auth";
-
-const ProtectedRoute = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
