1386d09094ca049ba5c00c662a1235335721d3aa
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-09-19 00:04:58 +0530

    working on the sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,7 +2,9 @@ const Sidebar = () => {
   return (
     <>
       <div className="p-2 fixed bg-[var(--top-color)] h-[100%] overflow-hidden w-[250px] left-0 top-0">
-        <h2 className="text-white font-bold text-[40px] text-wrap">PB Expense Tracker</h2>
+        <h2 className="text-white font-bold text-[40px] text-wrap">
+          PB Expense Tracker
+        </h2>
       </div>
     </>
   );
