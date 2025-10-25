a43ae0294652d7600133f14e91a89f9d3d523b26
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-10-25 21:55:02 +0530

    updated the stylings in sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = (props) => {
     <>
       <div
         className={`p-2 fixed bg-[var(--top-color)] h-[100%] overflow-hidden ${
-          isOpen ? "w-[250px]" : "w-[80px]"
+          isOpen ? "w-[250px]" : "w-[70px]"
         } left-0 top-0 transition-[width] duration-300 ease-in-out`}
       >
         {
@@ -26,46 +26,66 @@ const Sidebar = (props) => {
             onClick={handleCloseArrowClick}
           />
         }
-        <Link className="flex gap-x-3 justify-start pt-10 items-center" to={"/"}>
+        <Link
+          className={`flex gap-x-3 pt-10 items-center ${
+            !isOpen ? "justify-center" : "justify-start "
+          }`}
+          to={"/"}
+        >
           <div>
             <img src={logo} className="w-[100%] block" />
           </div>
-          <h2 className={`text-white font-bold text-lg text-wrap text-center ${!isOpen? "hidden" : ""}`}>
+          <h2
+            className={`text-white font-bold text-lg text-wrap text-center ${
+              !isOpen ? "hidden" : ""
+            }`}
+          >
             PB Expense Tracker
           </h2>
         </Link>
         <div className="pt-5 flex flex-col gap-y-4">
           <Link
-            className="text-white text-lg items-center flex gap-x-4 py-2"
+            className={`text-white text-lg items-center flex gap-x-4 py-2 ${
+              !isOpen ? "justify-center" : "justify-left"
+            }`}
             to={"/"}
           >
             <MdOutlineSpaceDashboard className="inline" />{" "}
-            <span className={`${!isOpen? "hidden" : ""}`}>Dashboard</span>
+            <span className={`${!isOpen ? "hidden" : ""}`}>Dashboard</span>
           </Link>
           <Link
-            className="text-white text-lg items-center flex gap-x-4 py-2"
+            className={`text-white text-lg items-center flex gap-x-4 py-2 ${
+              !isOpen ? "justify-center" : "justify-left"
+            }`}
             to={"/add-expense"}
           >
             <FaCirclePlus className="inline" />{" "}
-            <span className={`${!isOpen? "hidden" : ""}`}>Add Expense</span>
+            <span className={`${!isOpen ? "hidden" : ""}`}>Add Expense</span>
           </Link>
           <p
-            className="text-white text-lg items-center flex gap-x-4 py-2 cursor-pointer"
+            className={`text-white text-lg items-center flex gap-x-4 py-2 cursor-pointer ${
+              !isOpen ? "justify-center" : "justify-left"
+            }`}
             to={"/add-expense"}
             onClick={() => {
               logout();
             }}
           >
-            <FiLogOut className="inline" /> <span className={`${!isOpen? "hidden" : ""}`}>Logout</span>
+            <FiLogOut className="inline" />{" "}
+            <span className={`${!isOpen ? "hidden" : ""}`}>Logout</span>
           </p>
         </div>
-        <div className="fixed bottom-0 pb-7 text-center">
+        <div
+          className={`absolute bottom-0 pb-7 text-center ${
+            !isOpen ? "left-1/2 transform -translate-x-1/2" : ""
+          }`}
+        >
           <Link
             to={"/profile"}
             className="text-white text-lg items-center flex gap-x-4"
           >
             <FaUserCircle className="text-white" />
-            <span className={`${!isOpen? "hidden" : ""}`}>Profile</span>
+            <span className={`${!isOpen ? "hidden" : ""}`}>Profile</span>
           </Link>
         </div>
       </div>
