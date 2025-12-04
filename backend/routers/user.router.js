0447ce8e3ae6151e4e87f159c188883656f6edd9
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2025-12-04 09:44:58 +0530

    removed the /me api

--- backend/routers/user.router.js
+++ backend/routers/user.router.js
@@ -8,5 +8,6 @@ const updateUserSchema = require("../validators/user.validator");
 router
   .route("/update")
   .put(authMiddleware, validate(updateUserSchema), userController.updateUser);
+router.route("/me").get(authMiddleware, userController.getUserProfile);
 
 module.exports = router;
