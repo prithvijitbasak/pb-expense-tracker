d323336b1e5a8c2d2e5e40f2356e900d024626c3
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2026-01-02 22:30:36 +0530

    updated to get the full name

--- frontend/src/components/WelcomeAddBanner.jsx
+++ frontend/src/components/WelcomeAddBanner.jsx
@@ -1,44 +1,16 @@
 import "../assets/styles/WelcomeAddBanner.css";
 import { Link } from "react-router-dom";
-import { API } from "../utils/auth";
-import { useEffect, useState } from "react";
 import { ShimmerBar } from "./ShimmerUI";
 import { useAuth } from "../context/AuthContext";
 
 const WelcomeAddBanner = () => {
-  const [name, setName] = useState("");
   const { user } = useAuth();
-
-  const fetchName = async () => {
-    const token = localStorage.getItem("token");
-
-    try {
-      const response = await fetch(`${API}/api/auth/me`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      if (response.ok) {
-        const data = await response.json();
-        setName(data.fullName);
-      } else {
-        console.log("Unable to fetch name");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchName();
-  }, []);
-
+  
   return (
     <div className="welcome-add-expense-banner">
       <div className="name-card">
         <p className="welcome-card-text">Welcome,</p>
-        <h3 className="profile-name">{!name ? <ShimmerBar /> : name}</h3>
+        <h3 className="profile-name">{!user?.fullName ? <ShimmerBar /> : user?.fullName}</h3>
       </div>
       <div className="add-expense-card">
         <p className="add-expense-card-text">Have spent money on something?</p>
